f05d259c6e114ff7d4aa6a18648c75aa26a1db10
83metra <jumo_004@mail.ru>
2024-11-18 22:02:16 +0200

    Домашнее задание по теме "Urls и Views. Функциональное и классовое представление."
    Цель: закрепить знания о маршрутах (urls) и представлениях (views) в Django.

--- module_18_1/UrbanDjango/UrbanDjango/urls.py
+++ module_18_1/UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import welcome, Welcome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('welcome/', welcome),
+    path('goodbye/', Welcome.as_view())
 ]
